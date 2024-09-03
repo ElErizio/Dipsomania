f3b154bb9a55a3704e358cf4fe83a9e1896b7521
Eric I. Rosas <theericyea100@hotmail.com>
2024-09-02 18:48:51 -0600

    Se actualizó el acelerometro
    Arreglamos el acelerómetro y la esfera ya no se eleva

--- Assets/Scripts/Acelerometro.cs
+++ Assets/Scripts/Acelerometro.cs
@@ -15,7 +15,7 @@ public class Acelerometro : MonoBehaviour
 
     void Update()
     {
-        Vector3 tilt = Input.acceleration;
+        Vector3 tilt = new Vector3 (Input.acceleration.x, 0, 0);
         tilt = Quaternion.Euler(90,0,0)*tilt;
         rb.AddForce(tilt*speed);
     }
