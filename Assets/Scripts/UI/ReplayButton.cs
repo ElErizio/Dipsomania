cf734c664626e3225fd10d9449cfb995051da4b0
Eric I. Rosas <theericyea100@hotmail.com>
2024-10-21 20:03:56 -0600

    Prefab Canvas
    Prefav Canvas con nuevas funciones de los botones

--- Assets/Scripts/UI/ReplayButton.cs
+++ Assets/Scripts/UI/ReplayButton.cs
@@ -5,6 +5,6 @@ public class ReplayButton : MonoBehaviour
 {
     public void ReplayGame()
     {
-        SceneManager.LoadScene("Test_Lvl1_V2");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
